4e7b4ae6ef06b367295ab8033d2086ac23df32c2
Tess <njerigitundu003@gmail.com>
2022-03-05 21:31:24 +0300

    Add calculations to js

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
-function akanName () {
-    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+function hello(){
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var year = document.getElementById("year").value;
     var month = document.getElementById("month").value;
-    var date = document.getElementById (date).value;
-    var male = document.getElementById(male).checked;
-    var female = document.getElementById(female).checked;
-    var century = parseInt(year.slice(o,2));
+    var day = document.getElementById ("day").value;
+    var male = document.getElementById("male").checked;
+    var female = document.getElementById("female").checked;
+    var century = parseInt(year.slice(0,2));
     var year = parseInt(year.slice(2,4));
     var month = parseInt(month);
-    var date = parseInt(date);
-    var day =  ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date )%7
-    if (male === true) {
-        alert("Your Akan name is ")
-    } else (female === true) {
-        alert("Your Akan name is ")
+    var day = parseInt(day);
+    var weekDay =  Math.ceil(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day )%7);
+    
+    if (day<0 || day>31 || day=="") {
+        alert("Invalid day");
+    } else if (month<0 || month>12 || month=="" || month==2 && day>29) {
+        alert("Invalid month");
+    } else if (year<=0 || year=="") {
+        alert ("Invalid year.");
     }
+
+    if (male===true) {
+        if (weekDay === 1) {
+            alert("Your Akan name is: "+ maleNames[0]);
+        } else if (weekDay === 2) {
+            alert("Your Akan name is: "+ maleNames[1]);
+        } else if (weekDay === 3) {
+            alert("Your Akan name is: "+ maleNames[2]);
+        }else if (weekDay === 4) {
+            alert("Your Akan name is: "+ maleNames[3]);
+        }else if (weekDay === 5) {
+            alert("Your Akan name is: "+ maleNames[4]);
+        }else if (weekDay === 6) {
+            alert("Your Akan name is: "+ maleNames[5]);
+        }else if (weekDay === 7) {
+            alert("Your Akan name is: "+ maleNames[6]);
+        }
+    } else if (female===true) {
+        if (weekDay === 1) {
+            alert("Your Akan name is: "+ femaleNames[0]);
+        } else if (weekDay === 2) {
+            alert("Your Akan name is: "+ femaleNames[1]);
+        } else if (weekDay === 3) {
+            alert("Your Akan name is: "+ femaleNames[2]);
+        }else if (weekDay === 4) {
+            alert("Your Akan name is: "+ femaleNames[3]);
+        }else if (weekDay === 5) {
+            alert("Your Akan name is: "+ femaleNames[4]);
+        }else if (weekDay === 6) {
+            alert("Your Akan name is: "+ femaleNames[5]);
+        }else if (weekDay === 7) {
+            alert("Your Akan name is: "+ femaleNames[6]);
+        }
+    } 
+}
