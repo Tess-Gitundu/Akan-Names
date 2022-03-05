816f2b5c8ec73d50ac850295c12014cfb822505f
Tess <njerigitundu003@gmail.com>
2022-03-05 13:53:14 +0300

    Add function and variables

---
+++ script.js
@@ -0,0 +1,9 @@
+function akanName () {
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+    var year = document.getElementById("year").value;
+    var month = document.getElementById("month").value;
+    var date = document.getElementById (date).value;
+    var male = document.getElementById(male).checked;
+    var female = document.getElementById(female).checked
+}
